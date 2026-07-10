4a7af6f1dc8f9411987b7e2af2d6e05d97700820
joaonetrebka <joaopednet@gmail.com>
2026-07-10 18:16:03 -0300

    Adiciona login com sessão

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask , render_template, request
-from werkzeug.security import generate_password_hash
+from flask import Flask , render_template, request , session, redirect
+from werkzeug.security import generate_password_hash , check_password_hash
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = "therezinha"
 
 @app.route("/")
 def home():
@@ -28,6 +29,29 @@ def cadastro():
         except sqlite3.IntegrityError:
             return render_template("cadastro.html", mensagem="Erro: Email já cadastrado.")
     return render_template("cadastro.html", mensagem="")
-    
+
+#ROTA LOGIN
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        senha = request.form["senha"]
+
+        conexao = sqlite3.connect("vgc.db")
+        cursor = conexao.cursor()
+        cursor.execute("SELECT id, nome, senha_hash FROM usuarios WHERE email = ?", (email,))
+        usuario = cursor.fetchone()
+        conexao.close()
+
+        if usuario and check_password_hash(usuario[2], senha):
+            session["id_usuario"] = usuario[0]
+            session["nome"] = usuario[1]
+            return redirect("/times")
+        else:
+            return render_template("login.html", mensagem="Email ou senha incorretos.")
+        
+    return render_template("login.html", mensagem="")
+
+#DEBUG   
 if __name__ == "__main__":
     app.run(debug=True)
